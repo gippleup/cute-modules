c08e54ac5aa7c76bb8a4532648fb202189d90f70
monthem <lpoeh01@gmail.com>
2020-04-03 11:15:31 +0900

    Add Tree

--- data-structure/tree/Tree.js
+++ data-structure/tree/Tree.js
@@ -1,3 +1,5 @@
+const LinkedList = require('../linkedList/LinkedList');
+
 class Tree {
   constructor(value) {
     this.value = value;
@@ -5,43 +7,77 @@ class Tree {
     this.count = 0;
   }
 
+  /**
+   * @param {Tree} value if value is an instance of tree, push to this.children.
+   * @param {any} value if value is not an instance of tree, make new tree and push.
+   */
   insert(value) {
     const newTree = new Tree(value);
     this.children.push(newTree);
     this.count += 1;
   }
 
-  dfs(callback) {
-    callback(this);
-    if (this.children[0]) {
-      for (let i = 0; i < this.count; i += 1) {
-        this.children[i].dfs(callback);
-      }
-    }
+  forEachChildren(callback) {
+    this.children.forEach((tree) => {
+      callback(tree);
+    });
   }
 
+  /**
+   * @param {boolean} skipHead;
+   */
+  dfs(callback) {
+    callback(this);
+    this.forEachChildren((tree) => {
+      tree.dfs(callback);
+    });
+  }
+
+  /**
+   * @param {boolean} skipHead;
+   */
   bfs(callback) {
     let curTree = this;
-    const queue = [];
-    while (queue[0]) {
+    const queue = new LinkedList();
+    queue.insert(curTree);
+    while (queue.head) {
+      curTree = queue.removeHead().value; // node recieved tree as a value.
       callback(curTree);
-      if (curTree.children[0]) {
-        for (let i = 0; i < curTree.count; i += 1) {
-          queue.push(curTree.children[i]);
-        }
-      }
-      curTree = queue.unshift();
+      curTree.forEachChildren((tree) => {
+        queue.insert(tree);
+      });
     }
   }
 
   map(callback) {
     const newTree = new Tree(callback(this));
-    if (this.children[0]) {
-      for (let i = 0; i < this.count; i += 1) {
-        this.addChild(this.children[i].map(callback));
-      }
-    }
+    this.forEachChildren((tree) => {
+      newTree.children.push(tree.map(callback));
+      newTree.count += 1;
+    });
     return newTree;
+  }
+
+  filter(callback) {
+    const newTree = new Tree();
+    this.bfs((tree) => {
+      if (callback(tree)) {
+        if (!newTree.value) {
+          newTree.value = tree.value;
+        } else {
+          newTree.insert(tree.value);
+        }
+      }
+    });
+    return newTree;
+  }
+
+  reduce(callback, initialValue) {
+    let result = initialValue;
+    this.bfs((tree) => {
+      result = callback(result, tree);
+    });
+    return result;
   }
 }
 
--- data-structure/tree/Tree.test.js
+++ data-structure/tree/Tree.test.js
@@ -13,10 +13,10 @@ describe('Tree', () => {
     myTree.children[0].insert('grandChild2');
     myTree.children[1].insert('grandChild3');
     myTree.children[2].insert('grandChild4');
-    myTree.children[3].insert('grandChild4');
     myTree.children[3].insert('grandChild5');
     myTree.children[3].insert('grandChild6');
-    myTree.children[4].insert('grandChild7');
+    myTree.children[3].insert('grandChild7');
+    myTree.children[4].insert('grandChild8');
   });
 
   it('can add subTree', () => {
@@ -24,33 +24,92 @@ describe('Tree', () => {
     expect(myTree.children[0].children[0].value).toBe('grandChild1');
   });
 
+  it('can callback for each direct children', () => {
+    const testingValue = [];
+    myTree.forEachChildren((tree) => {
+      testingValue.push(tree.value);
+    });
+    const expectedValue = ['child1', 'child2', 'child3', 'child4', 'child5'];
+    expect(testingValue).toEqual(expectedValue);
+
+  });
+
   it('can callback for each tree with depth-first search', () => {
     const testingValue = [];
-    const allElement = ['origin',
+    const expectedValue = ['origin',
       'child1', 'grandChild1', 'grandChild2',
       'child2', 'grandChild3',
       'child3', 'grandChild4',
-      'child4', 'grandChild4', 'grandChild5', 'grandChild6',
-      'child5', 'grandChild7'];
+      'child4', 'grandChild5', 'grandChild6', 'grandChild7',
+      'child5', 'grandChild8',
+    ];
     myTree.dfs((tree) => {
       testingValue.push(tree.value);
     });
-    expect(testingValue).toEqual(allElement);
+    expect(testingValue).toEqual(expectedValue);
   });
 
-  it('can callback for each tree with broadth-first search', () => {
+  it('can callback for each tree with breadth-first search', () => {
     const testingValue = [];
-    const allElement = ['origin',
-      'child1', 'child2', 'child2', 'child3', 'child4', 'child5',
+    const expectedValue = ['origin',
+      'child1', 'child2', 'child3', 'child4', 'child5',
       'grandChild1', 'grandChild2', 'grandChild3', 'grandChild4',
-      'grandChild5', 'grandChild6', 'grandChild7',
+      'grandChild5', 'grandChild6', 'grandChild7', 'grandChild8',
     ];
     myTree.bfs((tree) => {
       testingValue.push(tree.value);
     });
-    expect(testingValue).toEqual(allElement);
+    expect(testingValue).toEqual(expectedValue);
   });
 
+  it('can map new tree', () => {
+    const testingValue = [];
+    const expectedValue = ['ori',
+      'chi', 'chi', 'chi', 'chi', 'chi',
+      'gra', 'gra', 'gra', 'gra',
+      'gra', 'gra', 'gra', 'gra',
+    ];
+    const newTree = myTree.map((tree) => tree.value.slice(0, 3));
+    newTree.bfs((tree) => {
+      testingValue.push(tree.value);
+    });
+    expect(testingValue).toEqual(expectedValue);
+  });
+
+  it('can filter element', () => {
+    const testingValue = [];
+    const expectedValue = ['origin',
+      'child1', 'child2', 'child3', 'child4', 'child5',
+    ];
+    const newTree = myTree.filter((tree) => tree.value[0] !== 'g');
+    newTree.bfs((tree) => {
+      testingValue.push(tree.value);
+    });
+    expect(testingValue).toEqual(expectedValue);
+  });
+
+  it('can perform reduce', () => {
+    const testingValue = myTree.reduce((acc, tree) => {
+      if (tree.value.indexOf('origin') > -1) {
+        acc.origin += 1;
+      } else if (tree.value.indexOf('child') > -1) {
+        acc.child += 1;
+      } else if (tree.value.indexOf('grandChild') > -1) {
+        acc.grandChild += 1;
+      }
+      return acc;
+    }, {
+      origin: 0,
+      child: 0,
+      grandChild: 0,
+    });
+    const expectedValue = {
+      origin: 1,
+      child: 5,
+      grandChild: 8,
+    };
+    expect(testingValue).toEqual(expectedValue);
+  });
 
   afterAll(() => {
     delete global.myTree;
